1ce2d6fb2dbcf649bbd00c0aa3d1ee1087477bae
Matheus Gritz <matheusgritz@gmail.com>
2020-07-29 23:11:18 -0300

    Save credentials to local storage

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -23,6 +32,9 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -45,6 +57,7 @@ const App = () => {
       {user === null
         ? loginForm()
         : <div>
+          <div>{user.name} logged in</div>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
