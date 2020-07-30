833b897973852e2172c8639b0ba18376c69623ed
Matheus Gritz <matheusgritz@gmail.com>
2020-07-30 00:39:52 -0300

    Implement blog creation form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  // const [newBlog, setNewBlog] = useState('')
+
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -18,7 +25,7 @@ const App = () => {
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
-    if(loggedUserJSON) {
+    if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
       blogService.setToken(user.token)
@@ -49,11 +56,35 @@ const App = () => {
     setUser(null)
   }
 
+  const addBlog = async (event) => {
+    event.preventDefault()
+
+    const blogObject = {
+      title,
+      author,
+      url
+    }
+
+    const returnedBlog = await blogService.create(blogObject)
+    setBlogs(blogs.concat(returnedBlog))
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
   const loginForm = () => (
     <LoginForm
       onSubmit={handleLogin}
       username={{ value: username, onChange: setUsername }}
       password={{ value: password, onChange: setPassword }} />
+  )
+
+  const blogForm = () => (
+    <BlogForm
+      onSubmit={addBlog}
+      title={{ value: title, onChange: setTitle }}
+      author={{ value: author, onChange: setAuthor }}
+      url={{ value: url, onChange: setUrl }} />
   )
 
   return (
@@ -62,8 +93,9 @@ const App = () => {
       {user === null
         ? loginForm()
         : <div>
-          <div>{user.name} logged in</div>
+          {user.name} logged in
           <button onClick={handleLogout}>Logout</button>
+          {blogForm()}
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
