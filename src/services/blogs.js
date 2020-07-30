b86682d0496fd371fdf8272192059947a8daf8c9
Matheus Gritz <matheusgritz@gmail.com>
2020-07-30 00:37:51 -0300

    Add post route to blog service

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -11,6 +11,13 @@ const getAll = async () => {
   const request = await axios.get(baseUrl)
   return request.data
 }
+
+const create = async (newObject) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
 }
 
-export default { getAll, setToken }
+export default { getAll, create, setToken }
