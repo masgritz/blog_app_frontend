495b5ab2b7586e7b9b0612a3888672d7b23bf19e
Matheus Gritz <matheusgritz@gmail.com>
2020-09-12 22:12:59 -0300

    Add Togglable component

---
+++ src/components/Togglable.js
@@ -0,0 +1,33 @@
+import React, { useState, useImperativeHandle } from 'react'
+
+const Togglable = React.forwardRef((props, ref) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const shownWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility
+    }
+  })
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      </div>
+
+      <div style={shownWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility}>Cancel</button>
+      </div>
+    </div>
+  )
+})
+
+export default Togglable
