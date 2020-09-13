7534163bc3b8409a6b922a3d7bb09ddc69aac68e
Matheus Gritz <matheusgritz@gmail.com>
2020-09-12 22:13:40 -0300

    Refactor LoginForm component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,36 @@
 import React from 'react'
 
-const LoginForm = ({ onSubmit, username, password }) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <div>
-        Username:
-        <input
-          type="text"
-          value={username.value}
-          name="Username"
-          onChange={({ target }) => username.onChange(target.value)} />
-      </div>
-      <div>
-        Password:
-        <input
-          type="password"
-          value={password.value}
-          name="Password"
-          onChange={({ target }) => password.onChange(target.value)} />
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  )
+const LoginForm = ({
+  handleSubmit,
+  handleUsernameChange,
+  handlePasswordChange,
+  username,
+  password
+ }) => {
+ return (
+   <div>
+     <h2>Login</h2>
+
+     <form onSubmit={handleSubmit}>
+       <div>
+         username
+         <input
+           value={username}
+           onChange={handleUsernameChange}
+         />
+       </div>
+       <div>
+         password
+         <input
+           type="password"
+           value={password}
+           onChange={handlePasswordChange}
+         />
+     </div>
+       <button type="submit">login</button>
+     </form>
+   </div>
+ )
 }
 
 export default LoginForm
